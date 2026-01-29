21f211fff6df9f7e282cdc305c93d750c980ca5a
Tyler Wilson <tyniweb@gmail.com>
2026-01-29 15:48:09 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,14 +5,14 @@ async function initAuth() {
     domain: "dev-fht8kl3tzpgoptkw.us.auth0.com", // e.g. dev-abc123.us.auth0.com
     client_id: "jzSlLP3cpq6AVAcWTf6YiLWySaGnNHgR", // e.g. jzS1LP3cpq64VAcWTf6YiLWySaGnNHgR
     authorizationParams: {
-      redirect_uri: window.location.origin + "/tyniweb/resume/portfolio.html"
+      redirect_uri: window.location.origin + "/tyniweb/portfolio.html"
     }
   });
 
   const query = window.location.search;
   if (query.includes("code=") && query.includes("state=")) {
     await auth0Client.handleRedirectCallback();
-    window.history.replaceState({}, document.title, "/tyniweb/resume/portfolio.html");
+    window.history.replaceState({}, document.title, "/tyniweb/portfolio.html");
   }
 }
 
