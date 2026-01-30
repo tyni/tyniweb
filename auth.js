d171cf51ef7cca616311c58c2d92f5484728e43e
Tyler Wilson <tyniweb@gmail.com>
2026-01-29 18:04:33 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ console.log("auth.js loaded");
 let auth0Client = null;
 
 async function initAuth() {
+  console.log("Redirect URI:", window.location.origin + "/tyniweb/portfolio.html");
   auth0Client = await createAuth0Client({
     domain: "dev-fht8kl3tzpgoptkw.us.auth0.com", // e.g. dev-abc123.us.auth0.com
     client_id: "jzSlLP3cpq6AVAcWTf6YiLWySaGnNHgR", // e.g. jzS1LP3cpq64VAcWTf6YiLWySaGnNHgR
