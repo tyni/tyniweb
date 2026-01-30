370a736569cb40033bbeee40bb7f33a723d92d20
Tyler Wilson <tyniweb@gmail.com>
2026-01-29 23:44:05 -0700

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,6 +1,6 @@
 let auth0Client = null;
 
-async function protectPage() {
+async function initAuth() {
   auth0Client = await createAuth0Client({
     domain: "dev-fht8kl3tzpgoptkw.us.auth0.com",
     client_id: "jzSlLP3cpq6AVAcWTf6YiLWySaGnNHgR"
@@ -10,15 +10,10 @@ async function protectPage() {
 
   if (!isAuthenticated) {
     window.location.href = "login.html";
+    return;
   }
-}
 
-async function logout() {
-  await auth0Client.logout({
-    logoutParams: {
-      returnTo: window.location.origin + "/tyniweb/index.html"
-    }
-  });
+  logVisit();
 }
 
 async function logVisit() {
@@ -27,6 +22,7 @@ async function logVisit() {
   fetch("https://script.google.com/macros/s/AKfycbx_GM5iIAY1xaLJsKaArGUm6q98PL5UWWOwHn_8E2SN-203qFvI-EICZasfQMsDmfvS/exec", {
     method: "POST",
     body: JSON.stringify({
+      type: "visit",
       email: user.email,
       userAgent: navigator.userAgent,
       page: "portfolio"
@@ -34,6 +30,12 @@ async function logVisit() {
   });
 }
 
-logVisit();
+document.getElementById("logoutBtn").onclick = () => {
+  auth0Client.logout({
+    logoutParams: {
+      returnTo: "https://tyni.github.io/tyniweb/index.html"
+    }
+  });
+};
 
-protectPage();
+initAuth();
